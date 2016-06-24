60d3dc80fada4eddfc03f0c75c42d1754b0860f6
TINA HAMILTON <tinabhamilton@gmail.com>
2016-06-23 22:37:46 -0400

    stuff

--- Charlotte Mills A Shop of Uncommon Goods - Facebook Search_files/ri7WLnEDQXN.js
+++
@@ -1,4 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js(["eedFS"]); }
-
-__d('legacy:uri',['URI'],function a(b,c,d,e,f,g){if(c.__markCompiled)c.__markCompiled();b.URI=c('URI');},3);
-__d('ShareNowConstants',['fbt'],function a(b,c,d,e,f,g,h){if(c.__markCompiled)c.__markCompiled();f.exports={error:h._("This post could not be shared at this time. Please try again later."),success:h._("Your share was posted.")};},null);
